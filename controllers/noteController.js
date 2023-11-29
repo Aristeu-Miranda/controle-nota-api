75fff1bc919a73b329d2ef44092f8b5460424b5f
Aristeu Miranda <aristeujose1999@gmail.com>
2023-11-29 16:59:48 -0300

    setting finalized

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,6 +1,6 @@
 const NoteModel = require("../models/note");
 const noteController = {
-    create: async (req, res) => {
+    create: async (req, res) => { //metodo POST criando uma nova inserção
         try {
             const note = {
                 name: req.body.name,
@@ -17,10 +17,58 @@ const noteController = {
             console.log(`Error: ${error}`)
         }
     },
-    getAll: async (req, res) => {
+    getAll: async (req, res) => { //pegando todos os dados do banco
         try {
             const notes = await NoteModel.find()
             res.json(notes)
+        } catch (error) {
+            console.log(`error: ${error}`)
+        }
+    },
+    get: async (req, res) => { //pegando só com base no ID pelo 'params'
+        try {
+            const id = req.params.id;
+            const notes = await NoteModel.findById(id)
+            if (!notes) {
+                res.status(404).json({ msg: "Registro não encontrado" });
+                return
+            }
+            res.json(notes)
+        } catch (error) {
+            console.log(`error: ${error}`)
+        }
+    },
+    update: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const note = {
+                name: req.body.name,
+                service: req.body.service,
+                contract: req.body.contract,
+                portion: req.body.portion,
+                status: req.body.status,
+                date: req.body.date,
+            };
+            const updateNote = await NoteModel.findByIdAndUpdate(id, note)
+            if(!updateNote) {
+                res.status(404).json({ msg: "Registro não encontrada" });
+                return
+            }
+            res.status(200).json({ note, msg: "Registro atualizado" })           
+        } catch (error) {
+            console.log(`error: ${error}`)
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const notes = await NoteModel.findById(id)
+            if (!notes) {
+                res.status(404).json({ msg: "Registro não encontrado" });
+                return
+            }
+            const deletedNote = await NoteModel.findByIdAndDelete(id)
+            res.status(200).json({ deletedNote, msg: "Registro removido" })
         } catch (error) {
             console.log(`error: ${error}`)
         }
--- routes/note.js
+++ routes/note.js
@@ -3,5 +3,8 @@ const noteController = require("../controllers/noteController");
 
 router.route("/notes").post((req, res) => noteController.create(req, res));
 router.route("/notes").get((req, res) => noteController.getAll(req, res));
+router.route("/notes/:id").get((req, res) => noteController.get(req, res));
+router.route("/notes/:id").put((req, res) => noteController.update(req, res));
+router.route("/notes/:id").delete((req, res) => noteController.delete(req, res));
 
 module.exports = router;
